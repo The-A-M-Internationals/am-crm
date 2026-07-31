dc74b71cf04bb0c29b779f24ce55a636437a598e
Narayanan-Vaithiyanathan <Narayanan.Vaithiyanathan@theaminternational.com>
2026-07-31 19:10:30 +0530

    fix: override webpack to force firebase-admin as external

--- next.config.mjs
+++ next.config.mjs
@@ -3,8 +3,12 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  experimental: {
-    serverComponentsExternalPackages: ["firebase-admin"],
+  webpack: (config, { isServer }) => {
+    if (isServer) {
+      // Force firebase-admin to be treated as an external package
+      config.externals.push("firebase-admin");
+    }
+    return config;
   },
 };
 
