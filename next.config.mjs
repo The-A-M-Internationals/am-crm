68856a0b81112ee6389b1ddc2edfb20fff49d218
Narayanan-Vaithiyanathan <Narayanan.Vaithiyanathan@theaminternational.com>
2026-08-03 19:27:13 +0530

    fix: use serverExternalPackages for Next.js 14 Vercel deployment

--- next.config.mjs
+++ next.config.mjs
@@ -3,13 +3,7 @@ const nextConfig = {
   images: {
     domains: ["firebasestorage.googleapis.com"],
   },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      // Force firebase-admin to be treated as an external package
-      config.externals.push("firebase-admin");
-    }
-    return config;
-  },
+  serverExternalPackages: ["firebase-admin"],
 };
 
 export default nextConfig;
